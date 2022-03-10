ca314af63df4c5c1ae48f6fbc4adfc269ce9360d
Daniel Kang <daniel.kang@edgezero.co>
2022-03-10 16:19:04 +1100

    draw menu updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Body from './components/body/Body';
-import Header from './components/header/Header';
+// import Body from './components/body/Body';
+import DrawNav from './components/body/common/draw-nav/DrawNav';
+// import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <DrawNav />
     </div>
   );
 }
---
+++ src/components/body/common/draw-nav/DrawNav.js
@@ -0,0 +1,39 @@
+import Header from '../../../header/Header';
+import Body from '../../Body';
+import './DrawNav.css';
+
+export default function DrawNav () {
+    return (
+        <div className='Drawer-nav'>
+            <input type="checkbox" id="drawer-toggle" name="drawer-toggle"/>
+            <label for="drawer-toggle" id="drawer-toggle-label"></label>
+            <nav id="drawer">
+                <ul>
+                <li id='list-title'>FLEET VIEW</li>
+                <li><a href="#">Commercial Fleet</a></li>
+                <li><a href="#">Advanced Fleet</a></li>
+                <li><a href="#">Residential Fleet</a></li>
+                <li id='list-title'>DEVICE MANAGEMENT</li>
+                <li><a href="#">Register Device</a></li>
+                <li><a href="#">Unassigned Device</a></li>
+                <li><a href="#">Activate SIM</a></li>
+                <li><a href="#">Retired Device</a></li>
+                <li id='list-title'>USER MANAGEMENT</li>
+                <li><a href='#'>Partner Management</a></li>
+                <li id='list-title'>NETWORK MANAGEMENT</li>
+                <li><a href="#">Live Loading Map</a></li>
+                <li><a href="#">Network Aggregated</a></li>
+                <li id='list-title'>ADMIN CONFIGURATION</li>
+                <li><a href="#">Device Status</a></li>
+                <li><a href="#">Register New Business</a></li>
+                <li><a href="#">High Voltage Dashboard</a></li>
+                <li><a href="#">High Voltage Opportunities</a></li>
+                </ul>
+            </nav>
+            <div id="page-content">
+                <Header />
+                <Body />
+            </div>
+        </div>
+    );
+}
