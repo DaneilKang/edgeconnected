82d3263a0f2b2ffc285507e69a8c8f2079fae350
DaneilKang <101069614+DaneilKang@users.noreply.github.com>
2022-03-15 16:15:46 +1100

    Updates

---
+++ precache-manifest.966df2b44abca2a22b24e48fe6ea0ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5691c9348043849d303a",
+    "url": "/edgeconnected/static/css/main.312ef0cb.chunk.css"
+  },
+  {
+    "revision": "5691c9348043849d303a",
+    "url": "/edgeconnected/static/js/main.5691c934.chunk.js"
+  },
+  {
+    "revision": "fbd0924dd8ee322e9bc2",
+    "url": "/edgeconnected/static/js/1.fbd0924d.chunk.js"
+  },
+  {
+    "revision": "c4aefabab065cd51238e",
+    "url": "/edgeconnected/static/js/2.c4aefaba.chunk.js"
+  },
+  {
+    "revision": "d0471a577b1aa6c9953f",
+    "url": "/edgeconnected/static/js/runtime~main.d0471a57.js"
+  },
+  {
+    "revision": "2c71a54d94c8118e24f7ec2839a9e057",
+    "url": "/edgeconnected/index.html"
+  }
+];
