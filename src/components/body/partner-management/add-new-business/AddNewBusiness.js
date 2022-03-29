9664445354f6175177f65146ceae3375e73ee18a
DaneilKang <101069614+DaneilKang@users.noreply.github.com>
2022-03-29 11:16:06 +1100

    added add-new-business

--- src/components/body/partner-management/add-new-business/AddNewBusiness.js
+++ src/components/body/partner-management/add-new-business/AddNewBusiness.js
@@ -3,9 +3,20 @@ import { useForm } from 'react-hook-form';
 import styles from './AddNewBusiness.module.css';
 
 function AddNewBusiness ({setShowModal}) {
-
+    
     const {register, handleSubmit} = useForm();
+        
+    const formDataField = [  {'formTitle':'Name', 'formName':'name', 'formType':'text', 'isNeeded':true}, 
+                        {'formTitle':'Phone', 'formName':'phone_number', 'formType':'text', 'isNeeded':true}, 
+                        {'formTitle':'Address', 'formName':'address', 'formType':'text', 'isNeeded':true}, 
+                        {'formTitle':'City', 'formName':'city', 'formType':'text', 'isNeeded':false}, 
+                        {'formTitle':'Postcode', 'formName':'post_code', 'formType':'text', 'isNeeded':false}, 
+                        {'formTitle':'State', 'formName':'state', 'formType':'text', 'isNeeded':false},
+                        {'formTitle':'Country', 'formName':'country', 'formType':'text', 'isNeeded':false}];
+    
     const onSubmit = (data) => {
+
+        const newData = {...data, admin_id: JSON.parse(localStorage.role).role_id}; // add admin_id to data
             
         // get the authendication token
         const USER_TOKEN = localStorage.getItem('jwtToken');
@@ -17,62 +28,35 @@ function AddNewBusiness ({setShowModal}) {
                 'Content-Type': 'application/json',
                 'x-token': USER_TOKEN
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(newData)
         };
 
-        console.log(data);
+        console.log("data : ", newData);
         fetch(url, config)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(newData => console.log(newData));
 
         setShowModal(false);
     }
-    
-    const fake_roles = [{'role_id': 1, 'role_name': 'Wholesale Distributor'},
-                        {'role_id': 2, 'role_name': 'Partner'},
-                        {'role_id': 3, 'role_name': 'Installer'},
-                        {'role_id': 4, 'role_name': 'Partner network'}];
-    
-    const formDataField = [  {'formTitle':'Name', 'formName':'name', 'formType':'text', 'isNeeded':true}, 
-                        {'formTitle':'Email', 'formName':'email', 'formType':'email', 'isNeeded':true}, 
-                        {'formTitle':'Phone', 'formName':'phone', 'formType':'number', 'isNeeded':true}, 
-                        {'formTitle':'Address', 'formName':'address', 'formType':'text', 'isNeeded':true}, 
-                        {'formTitle':'Company Logo', 'formName':'company_logo', 'formType':'text', 'isNeeded':false}, 
-                        {'formTitle':'Website', 'formName':'website', 'formType':'text', 'isNeeded':false}, 
-                        {'formTitle':'Customer Service Email', 'formName':'customer_service_email', 'formType':'email', 'isNeeded':false}, 
-                        {'formTitle':'Customer Service Phone', 'formName':'customer_service_phone', 'formType':'number', 'isNeeded':false}, 
-                        {'formTitle':'Customer Service Url', 'formName':'customer_service_url', 'formType':'text', 'isNeeded':false}, 
-                        {'formTitle':'Partner Rep Email', 'formName':'partner_rep_email', 'formType':'text', 'isNeeded':false}, 
-                        {'formTitle':'Memo', 'formName':'memo', 'formType':'text', 'isNeeded':false}, 
-                        {'formTitle':'Profile Description', 'formName':'profile_description', 'formType':'text', 'isNeeded':false}];    
     
     return ( 
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.container}>
                 <div className={styles.title}>Register Business</div>
                 <div className={styles.form}>
-                    <div>
-                        <label htmlFor="role">Role</label>
-                        <select className={styles.role} name='role' id='role' {...register("role", { required: true })}>
-                                <option value="" disabled defaultValue>Select Role</option>
-                            {fake_roles.map((role, index) => (                        
-                                <option id={index} value={role.role_id}>{role.role_name}</option>
-                            ))}
-                        </select>
-                    </div>
                     <div className={styles.name}>
                         {formDataField.map((field, index) => (
                             <>
-                                <div><label htmlFor={field.formName}>{field.formTitle}</label></div>
+                                <div className={styles.col_25}><label htmlFor={field.formName}>{field.formTitle}</label></div>
                                 <div><input {...register(field.formName, { required: true })} type={field.formType} id={index} placeholder={field.formName}/></div>
                             </>
                             )
                         )}                   
                     </div>
-                </div>
-                <div className={styles.buttons}>
-                    <button className={styles.button} onClick={onSubmit}>Add New</button>
-                    <button className={styles.button} onClick={()=>setShowModal(false)}>Close</button>
+                    <div className={styles.buttons}>
+                        <button className={styles.button} onClick={onSubmit}>Add New</button>
+                        <button className={styles.button} onClick={()=>setShowModal(false)}>Close</button>
+                    </div>
                 </div>
             </div>
         </form>
